892da8e2a361e6d1bfc8ce429a44fe7910377e83
Renan de Medeiros Porto <renan.porto1099@gmail.com>
2018-02-24 10:11:31 -0300

    Criando novo env

---
+++ envsoccer/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/renanporto-user/git/son-courses/django-soccer1/envsoccer/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
